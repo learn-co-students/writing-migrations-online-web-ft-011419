c3f9998cee926f4287182049ddb76e833e299942
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2019-03-05 15:19:01 -0500

    works through writing migrations

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
+#note: you cannot add new columns to the existing migration file!!
